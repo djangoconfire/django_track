dbc2eca4c86fed401132552081bcf5229c58c591
djangoconfire <djangopycon@gmail.com>
2017-04-17 11:31:30 +0530

    added requirements.txt

---
+++ static/static_root/js/home/config.route.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.home')
+        .config(configFunction);
+
+    configFunction.$inject = ['$routeProvider','STATIC_URL'];
+
+    function configFunction($routeProvider, STATIC_URL) {
+        $routeProvider.when('/', {
+            templateUrl: STATIC_URL + '/home/home.html'
+        });
+
+         // $locationProvider.html5Mode({
+         //         enabled: true,
+         //         requireBase: false
+         //  });
+    }
+})();
